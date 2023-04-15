79e4d91a34c85ee16d9e74f9395deb75d4e78ae9
Rohit Singh <RohitSinghEmail@protonmail.com>
2023-04-15 19:07:14 +0530

    flakes and home manager setup

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+  	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	home-manager.url = "github:nix-community/home-manager";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  
+  
+  outputs = inputs@{ nixpkgs, home-manager, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+    	inherit system;
+    	config.allowUnfree = true;
+    };
+    in {
+    	nixosConfigurations = {
+	        # Can have different conf for fifferent users
+    		rohits = nixpkgs.lib.nixosSystem {
+    			inherit system;
+    			modules = [ 
+			./configuration.nix 
+			home-manager.nixosModules.home-manager {
+				    home-manager.useGlobalPkgs = true;
+				    home-manager.useUserPackages = true;
+				    home-manager.users.rohits = import ./home.nix; 
+				  }
+			];
+    		};
+    	};
+
+    };
+
+
+  
+}
