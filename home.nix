704437f1dd90311a50da8f344b56b5e538d8eea0
Rohit Singh <RohitSinghEmail@protonmail.com>
2023-04-16 13:49:43 +0530

    minor change

--- home.nix
+++ home.nix
@@ -1,29 +1,53 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
+
+
+
   home.username = "rohits";
   home.homeDirectory = "/home/rohits";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "22.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
+  programs.git = {
+    enable = true;
+    userName = "Rohit Singh";
+    userEmail = "RohitSinghEmail@protonmail.com";
+  };
   home.packages = with pkgs; [
   alacritty
   fish
   htop
   gdu
+  bitwarden
+  bitwarden-cli
+  neofetch
+  gnomeExtensions.dash-to-dock
   ];
 
+
+
+gtk = {                                     # Theming
+    enable = true;
+    theme = {
+      name = "Dracula";
+      #name = "Catppuccin-Mocha-Compact-Mauve-Dark";
+      package = pkgs.dracula-theme;
+      #package = pkgs.catppuccin-gtk.override {
+      #  accents = ["mauve"];
+      #  size = "compact";
+      #  variant = "mocha";
+      #};
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    font = {
+      #name = "JetBrains Mono Medium";
+      name = "FiraCode Nerd Font Mono Medium";
+    };                                        # Cursor is declared under home.pointerCursor
+  };
 }
