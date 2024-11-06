6ddec0ccc9d288690f74beedf3a772d2de95c7bb
Ubuntu <azureuser@scripts-vm.n0vongdjkhfutiwz3uklbllg4c.rx.internal.cloudapp.net>
2024-11-06 17:57:55 +0000

    home file azureuse

--- flake.nix
+++ flake.nix
@@ -108,6 +108,14 @@
           ./home-manager/opensuse-hp/rohit/home.nix
         ];
       };
+      "azureuser@scripts-vm" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/cloud-vms/scripts-vm/azureuser.nix
+        ];
+      };
     };
   };
 }
