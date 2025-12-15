9a918fd51760f0b1770835380f4b391e17a1a99f
Rohit Singh <RohitSinghEmail@protonmail.com>
2025-12-15 09:48:25 +0000

    nixpkgs

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     # TODO: Add any other flake you might need
